f78a0123907afe717c1e96c1cc7a77aa70f9296a
Davi Lisboa <129395367+davi-ldf@users.noreply.github.com>
2024-04-02 23:50:48 -0300

    Leitura de Posts
    Este pedaço do JS é dedicado a ler cada um dos posts da API e os trazer para a tela do Blog.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,25 @@
 // https://jsonplaceholder.typicode.com/posts
 
+async function readPosts() {
+    let postArea = document.querySelector('.posts');
+    postArea.innerHTML = 'Loading...';
 
+    let response = await fetch('https://jsonplaceholder.typicode.com/posts');
+    let json = await response.json();
+
+    if(json.length > 0) { //se houverem posts
+        postArea.innerHTML = ''; //o 'Loading...' será limpo
+
+        for( let i in json) {
+        //será dado um loop em cada um dos posts
+            let postHtml = `<div><h1>${json[i].title}</h1>${json[i].body}</hr></div>`;
+            //serão chamados os títulos e o bodies de cada um dos posts
+            postArea.innerHTML += postHtml;
+        }
+    } else { //senão
+        postArea.innerHTML = 'No posts to show'
+    }
+}
+
+
+readPosts();
