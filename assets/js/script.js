103f31a38600f321aac9bc513ad3a6050f410bb5
Davi Lisboa <129395367+davi-ldf@users.noreply.github.com>
2024-04-03 00:15:00 -0300

    Função de Adicionar Posts ao Blog
    O Blog está completo, agora com opção de adicionar novos posts implementada.

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,7 +13,7 @@ async function readPosts() {
         for( let i in json) {
         //será dado um loop em cada um dos posts
             let postHtml = `<div><h1>${json[i].title}</h1>${json[i].body}</hr></div>`;
-            //serão chamados os títulos e o bodies de cada um dos posts
+            //serão chamados os títulos e o corpos de cada um dos posts
             postArea.innerHTML += postHtml;
         }
     } else { //senão
@@ -21,5 +21,94 @@ async function readPosts() {
     }
 }
 
+async function addNewPost(title, body) {
+    await fetch( //faz a requisição POST
+        'https://jsonplaceholder.typicode.com/posts',
+        {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                title,
+                body,
+                userId: 2
+            })
+        }
+        //Envia título e corpo digitados para a requisição POST
+    );
 
-readPosts();
+    document.querySelector('#titleField').value = '';
+    document.querySelector('#bodyField').value = '';
+    //Limpo os campos
+    
+    readPosts();
+    //Lê os posts
+
+}
+
+document.querySelector('#insertButton').addEventListener('click', () => {
+    let title = document.querySelector('#titleField').value;
+    let body = document.querySelector('#bodyField').value;
+    //pega os valores do título e do corpo do post
+
+    if(title && body) { //se houverem título e corpo digitados
+        addNewPost(title, body); //pode postar
+
+    } else { //senão
+        alert('Fill in all fields.')
+        //alerta para o usuário
+    }
+
+})
+
+async function listPosts() {
+    const url = "https://jsonplaceholder.typicode.com/posts";
+    return fetch(url).then((response) => response.json());
+  }
+  
+  async function addPost(title, body) {
+    const url = "https://jsonplaceholder.typicode.com/posts";
+    const request = {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ title, body }),
+    };
+  
+    return fetch(url, request).then((response) => response.json());
+  }
+  
+  function renderPostList(list, posts) {
+    list.innerHTML = posts
+      .map(({ id, title, body }) => {
+        return `
+        <article id="post-${id}">
+          <h2>${title}</h2>
+          ${body}
+        </article>
+      `;
+      })
+      .join("");
+  }
+  
+  async function app() {
+    const [postList, titleField, bodyField, addButton] = [
+      ".posts",
+      "#titleField",
+      "#bodyField",
+      "#insertButton",
+    ].map((selector) => document.querySelector(selector));
+  
+    const posts = await listPosts();
+    addButton.addEventListener("click", async () => {
+      posts.unshift(await addPost(titleField.value, bodyField.value));
+      titleField.value = "";
+      bodyField.value = "";
+  
+      renderPostList(postList, posts);
+    });
+  
+    renderPostList(postList, posts);
+  }
+  
+  app();
